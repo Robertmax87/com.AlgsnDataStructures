1f6523cb9ffcd2dd19ebed06fc233cb968dc04ec
RobertMax87 <robertmaxwell133@gmail.com>
2021-07-13 13:46:39 -0700

    Initial commit

--- src/LinkedListQuestions/BinaryTreeLL.java
+++ src/LinkedListQuestions/BinaryTreeLL.java
@@ -52,8 +52,19 @@ public class BinaryTreeLL {
     //will not take any parameters
     public void levelOrder() {
         Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        //root is at first level so visit root first
+        //add the first element to the binary tree
         queue.add(root);
+        //visit all the nodes; uses O(N) time complexity
+        // while the Queue is not empty
         while (!queue.isEmpty()) {
+            /**While queue is not empty add and remove elements from the queue.
+             removes the first value, then adds two values, removes right first
+             then adds two values removes left, then adds two values removes right;
+             then keeps removing values and printing values once all of the values are added...
+             *
+             */
+
             BinaryNode present = queue.remove();
             System.out.print(present.value);
             if (present.left != null) {
@@ -63,6 +74,35 @@ public class BinaryTreeLL {
                 queue.add(present.right);
             }
         }
+    }
+
+    /**
+     * For a Binary Search use level order traversal on order to search all nodes and levels
+     * to find the nodes you're looking for.
+     */
+    public void search(BinaryNode node) {
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            BinaryNode presentNode = queue.remove();
+            /**This part is needed in order to traverse the tree and check to see if the node we
+             * are looking for is the one we are looking at. PresentValue is a node we are removing
+             * then looking at/checking to see if it is our value.
+             */
+            if (presentNode.value.equals(node.value)) {
+                System.out.print("The Node is found");
+                return;
+            } else {
+                if (presentNode.left != null){
+                    queue.add(presentNode.left);
+                }
+                if(presentNode.right != null){
+                    queue.add(presentNode.right);
+                }
+            }
+            System.ar
+        }
+        System.out.print("The node isn't found");
     }
 }
 
--- src/StackImplimentation/MainStacks.java
+++ src/StackImplimentation/MainStacks.java
@@ -20,5 +20,13 @@ public class MainStacks {
         return false;
 
     }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0){
+            return;
+        } else if (m == 1)
+            System.arraycopy(nums2,0,nums1,0,1);
+
+    }
+
 
 }
