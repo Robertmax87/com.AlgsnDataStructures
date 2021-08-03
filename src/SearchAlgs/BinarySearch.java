087394a3ac849b635f1c25130495cd3337f25ffb
RobertMax87 <robertmaxwell133@gmail.com>
2021-08-03 14:26:38 -0700

    Latest commit

--- src/SearchAlgs/BinarySearch.java
+++ src/SearchAlgs/BinarySearch.java
@@ -1,4 +1,27 @@
 package SearchAlgs;
 
 public class BinarySearch {
+    public static void main(String[] args){
+
+    }
+    public static int binarySearch(int[] nums, int target){
+    int start = 0;
+    // the last index in any array...
+    int end = nums.length - 1;
+    int mid = (start + end)/ 2;
+    while(nums[mid] != target){
+        if(nums[mid] < target){
+            //keep moving mid down from top
+            end = mid -1;
+
+
+        } else{
+            //keep moving mid up from bottom;
+            start = mid + 1;
+        }
+        //update mid
+         mid = (start + end)/ 2;
+    }
+    return mid;
+    }
 }
