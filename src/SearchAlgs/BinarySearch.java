b5ddf8bad504ae9e19429434abffd360ce408844
RobertMax87 <robertmaxwell133@gmail.com>
2021-08-03 16:17:21 -0700

    Latest commit

--- src/SearchAlgs/BinarySearch.java
+++ src/SearchAlgs/BinarySearch.java
@@ -1,27 +1,56 @@
 package SearchAlgs;
 
 public class BinarySearch {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        int[] garden = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 23, 54, 65};
+        System.out.print(biSearcher(garden, 23));
     }
-    public static int binarySearch(int[] nums, int target){
-    int start = 0;
-    // the last index in any array...
-    int end = nums.length - 1;
-    int mid = (start + end)/ 2;
-    while(nums[mid] != target){
-        if(nums[mid] < target){
-            //keep moving mid down from top
-            end = mid -1;
+
+    public static int binarySearch(int[] nums, int target) {
+        int start = 0;
+        // the last index in any array...
+        int end = nums.length - 1;
+        int mid = (start + end) / 2;
+        while (nums[mid] != target ) {
+            if (target < nums[mid]) {
+                //keep moving mid down from top
+                end = mid - 1;
 
 
-        } else{
-            //keep moving mid up from bottom;
-            start = mid + 1;
+            } else {
+                //keep moving mid up from bottom;
+                start = mid + 1;
+            }
+            //update mid
+            mid = (start + end) / 2;
+
         }
-        //update mid
-         mid = (start + end)/ 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else {
+            return 9;
+        }
     }
-    return mid;
+    public static int biSearcher(int nums[], int target){
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = (start + end) / 2;
+        while(nums[mid] != target && start < end){
+            if(target < nums[mid]){
+                // to inquire about the first half of the array
+                end = mid - 1;
+            }
+            else {
+                //to inquire about the second half of the array
+                start = mid + 1;
+            }
+            mid = (start + end) / 2;
+        }
+        if (nums[mid] == target){
+            return mid;
+        }
+        else{
+            return -1;
+        }
     }
 }
