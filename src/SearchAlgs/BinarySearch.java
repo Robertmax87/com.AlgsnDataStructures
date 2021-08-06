0f024c7c683400c0db364d5e08e4c1f04146e6d8
RobertMax87 <robertmaxwell133@gmail.com>
2021-08-06 14:38:07 -0700

    Latest commit

--- src/SearchAlgs/BinarySearch.java
+++ src/SearchAlgs/BinarySearch.java
@@ -53,4 +53,49 @@ public class BinarySearch {
             return -1;
         }
     }
+    public static int biSearch(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = (start + end)/2;
+        while(nums[mid] != target && start < end){
+            if (nums[mid] < target){
+                end = mid -1;
+            }
+            else{
+                start = mid + 1;
+            }
+            mid = (start + end)/2;
+        }
+        if (nums[mid] == target){
+            return mid;
+        }
+        else{
+            return -1;
+        }
+    }
+    public static void merge(int[] nums, int start, int end){
+        if(end - start < 2){
+            return;
+        }
+        int mid = (start + end) / 2;
+        merge(nums,start,mid);
+        merge(nums, mid, end);
+        sort(nums, start, mid, end);
+
+
+    }
+    public static void sort(int[] nums, int start, int mid, int end){
+        if (nums[mid - 1] <= nums[mid]){
+            return;
+        }
+        int i = start;
+        int j = end;
+        int tempIndex = 0;
+        int[] tempArray = new int[end - start];
+        while(i < mid && j < end){
+            tempArray[tempIndex++] = nums[i] <= nums[j] ? nums[i++] : nums[j++];
+        }
+        System.arraycopy(tempArray, i, nums, tempIndex + start, mid - i);
+        System.arraycopy(nums, 0, nums, start, tempIndex);
+    }
 }
